266de1af486401c1a0fcf14a1b45021c45e4a718
ferjc20 <128169722+ferjc20@users.noreply.github.com>
2024-09-21 18:36:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.body.addEventListener('click', function() {
     const video = document.getElementById('background-video');
-    if (video.muted) {
-        video.muted = false;
+    if (video.paused) {
+        video.muted = false; // Desactivar el mute solo si se hace clic
+        video.play().catch(error => {
+            console.error("Error al intentar reproducir el video: ", error);
+        });
     }
 });
